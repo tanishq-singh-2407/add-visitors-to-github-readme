06207196074335b22433d2cc415a6227eb27c0fe
Tanishq Singh <tanishqsingh640@gmail.com>
2022-10-04 16:26:44 +0530

    test 1

---
+++ .husky/add-name.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/bash
+
+NAME=$1
+
+echo "https://avatars.githubusercontent.com/$NAME"
---
+++ .husky/update-repo.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+git config --global user.name "Tanishq Singh"
+git config --global user.email "tanishq-singh-2301@users.noreply.github.com"
+git add .
+
+clean=$(git commit -m "CI: Updated visitors.json and README.md")
+
+if [[ ${clean} == *"nothing to commit"* ]]; then
+    echo "README.md already up to date."
+else
+    git push
+fi
