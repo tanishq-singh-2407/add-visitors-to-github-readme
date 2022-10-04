3d8ced8003cd1d6064e6240eb2fe59df06e9a989
Tanishq Singh <tanishqsingh640@gmail.com>
2022-10-04 18:37:22 +0530

    finally done

--- src/lib/render.ts
+++ src/lib/render.ts
@@ -6,10 +6,10 @@ const createReadmeText = () => `
 ## _**Visitors**_
 
 <p align="left">
-    ${data.visitors.map(id => `<a href="https://github.com/${id}"><img src="https://avatars.githubusercontent.com/${id}?s=96" width="96px" height="96px" /></a>`).join("\n    ")}
+    ${data.visitors.map(id => `<a target="_blank" rel="noopener" href="https://github.com/${id}"><img src="https://avatars.githubusercontent.com/${id}?s=96" width="96px" height="96px" /></a>`).join("\n    ")}
 </p>
 
-Don't see your badge? Then create an [_**issue.**_](https://github.com/tanishq-singh-2301/add-visitors-to-github-readme/issues/new?title=Add%20My%20Name)
+Don't see your badge? Then create an [_**issue.**_](https://github.com/tanishq-singh-2301/add-visitors-to-github-readme/issues/new?title=Add+My+Name&body=Jush+push+'Submit+new+issue'.+You+don't+need+to+do+anything+else.+wait+to+see+live+updated.)
 `;
 
 export const render = async () => await Deno.writeTextFile("./README.md", createReadmeText());
