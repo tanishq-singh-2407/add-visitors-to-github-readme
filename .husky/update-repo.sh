6ea10ea5fcbd5cb03ab09d597973409cce210c7b
Tanishq Singh <tanishqsingh640@gmail.com>
2022-10-04 18:25:37 +0530

    CI: Updated visitors.json and README.md

--- .husky/update-repo.sh
+++ .husky/update-repo.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
-git config --global user.name "Tanishq Singh"
-git config --global user.email "tanishq-singh-2301@users.noreply.github.com"
+# git config --global user.name "Tanishq Singh"
+# git config --global user.email "tanishq-singh-2301@users.noreply.github.com"
+
+git status
 git add .
 
 clean=$(git commit -m "CI: Updated visitors.json and README.md")
